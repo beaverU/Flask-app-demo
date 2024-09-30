67658d3fe0ffea33f52330184cc5fecc7fdb2a8e
beaverU <ivanklapyshev@gmail.com>
2024-09-30 13:03:06 +0400

    added readme and swagger docs

---
+++ README.md
@@ -0,0 +1,13 @@
+# Test Flask app to fun and learn
+It's just small pet project to learn Flask framework and to understend better RESTful API
+## How to run it
+- App uses **Poetry**
+    - It's a tool for managing dependencies and project packaging
+    - Currently I'm using it only for dependencies management, but in time, I hope, I would use packaging ability as well
+    - To install it hit this command in the terminal:
+        - `curl -sSL https://install.python-poetry.org | python3 -`
+    - Then run the program:
+        - `poetry run python server.py`
+
+# Swagger docs
+- To check app's API endpoinds you should run the program and reach `http://127.0.0.1:5000/apidocs`
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request
 import json
 from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask("Product Server")
 CORS(app)
-
+swagger = Swagger(app)
 
 products = [
     {'id': 143, 'name': 'Notebook', 'price': 5.49},
@@ -15,10 +16,31 @@ products = [
 # Example request - http://127.0.0.1:5000/products
 @app.route('/products', methods=['GET'])
 def get_products():
+    """
+    Get all products
+    ---
+    tags:
+      - Products
+    responses:
+      200:
+        description: List of products
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              id:
+                type: integer
+              name:
+                type: string
+              price: 
+                type: number
+                format: float
+    """
     return jsonify(products)
 
 # Example request - http://127.0.0.1:5000/products/144 - with method GET
-@app.route('/products/<id>', methods=['GET'])
+@app.route('/products/<int:id>', methods=['GET'])
 def get_product(id):
     id = int(id)
     product = [x for x in products if x["id"] == id][0]
@@ -27,12 +49,65 @@ def get_product(id):
 # Example request - http://127.0.0.1:5000/products - with method POST
 @app.route('/products', methods=['POST'])
 def add_product():
+    """
+    Create a new product
+    ---
+    tags:
+      - Products
+    parameters:
+      - in: body
+        name: body
+        schema:
+          type: object
+          required:
+            - name
+          properties:
+            id:
+              type: integer
+            name:
+              type: string
+            price:
+              type: number
+              format: float
+    responses:
+      201:
+        description: Product created
+    """
     products.append(request.get_json())
     return '', 201
 
 # Example request - http://127.0.0.1:5000/products/144 - with method PUT
-@app.route('/products/<id>', methods=['PUT'])
+@app.route('/products/<int:id>', methods=['PUT'])
 def update_product(id):
+    """
+    Update a product
+    ---
+    tags:
+      - Products
+    parameters:
+      - in: path
+        name: id
+        type: integer
+        required: true
+        description: The ID of the item
+      - in: body
+        name: body
+        schema:
+          type: object
+          properties:
+            id:
+              type: integer
+            name:
+              type: string
+            price:
+              type: number
+              format: float
+    responses:
+      204:
+        description: Product updated
+      404:
+        description: Product not found
+    """
     id = int(id)
     updated_product = json.loads(request.data)
     product = [x for x in products if x["id"] == id][0]
@@ -41,8 +116,25 @@ def update_product(id):
     return '', 204
 
 # Example request - http://127.0.0.1:5000/products/144 - with method DELETE
-@app.route('/products/<id>', methods=['DELETE'])
+@app.route('/products/<int:id>', methods=['DELETE'])
 def remove_product(id):
+    """
+    Delete a product
+    ---
+    tags:
+      - Products
+    parameters:
+      - in: path
+        name: id
+        type: integer
+        required: true
+        description: The ID of the product
+    responses:
+      204:
+        description: Product deleted
+      404:
+        description: Product not found
+    """
     id = int(id)
     product = [x for x in products if x["id"] == id][0]
     products.remove(product)
